9141df9ccc35e9757823f506b5f6d9e7fc9f80a6
Pawneet Singh <pawneet.dev@gmail.com>
2016-04-20 10:40:49 +0000

    Scrapes data from flipkart

--- my_scraper/items.py
+++ my_scraper/items.py
@@ -1,8 +1,14 @@
-import scrapy
+# import scrapy
+
+# from scrapy.item import Item, Field
+
+# class DmozItem(scrapy.Item):
+#     title = Field()
+#     link = Field()
+#     desc = Field()
 
 from scrapy.item import Item, Field
 
-class DmozItem(scrapy.Item):
-    title = Field()
-    link = Field()
-    desc = Field()
+class MyItem(Item):
+    bookname = Field()
+    price = Field()
--- my_scraper/spiders/dmoz_spider.py
+++ my_scraper/spiders/dmoz_spider.py
@@ -1,13 +1,35 @@
-import scrapy
+# import scrapy
 
-class DmozSpider(scrapy.Spider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
-    start_urls = ["http://www.dmoz.org/Computers/Programming/Languages/Python/Books/", "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
+# class DmozSpider(scrapy.Spider):
+#     name = "dmoz"
+#     allowed_domains = ["dmoz.org"]
+#     start_urls = ["http://www.dmoz.org/Computers/Programming/Languages/Python/Books/", "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
     
-    def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+#     def parse(self, response):
+#         filename = response.url.split("/")[-2] + '.html'
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+
+#     for sel in response.xpath('//ul/li'):
+#         title = sel.xpath('a/text()').extract()
+#         link = sel.xpath('a/@href').extract()
+#         desc = sel.xpath('text()').extract()
+#         print title, link, desc
+
+import scrapy
+# from scrapy.contrib.spiders import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+# from scrapBib.items import *
 
 
+class MySpider(scrapy.Spider):
+    name = "scraper"
+    allowed_domains = "flipkart.com"
+    start_urls = ['http://www.flipkart.com/search/a/all?query=mybook']
+
+    def parse(self,response):
+        hls = HtmlXPathSelector(response)
+        # it = MyItem()
+        it = hls.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "fk-display-block", " " ))]').extract()
+        # it1 = hls.xpath('//span[@price]/text()').extract()
+        print it
